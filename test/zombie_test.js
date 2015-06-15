eee32c2df7949782ad7e60cf92606bdeb489b122
proshunsuke <shunsuke0901@gmail.com>
2015-06-16 00:28:34 +0900

    iojsのファンクショナルテストの基本形できた

--- test/zombie_test.js
+++ test/zombie_test.js
@@ -1,21 +1,30 @@
-const Browser = require('zombie');
+process.env.NODE_ENV = 'test';
 
-// We're going to make requests to http://example.com/signup
-// Which will be routed to our test server localhost:3000
-Browser.localhost('example.com', 3000);
+let app = require('../app');
+let http = require('http');
 
-describe('User visits signup page', function() {
+let Browser = require('zombie');
+describe('User visits signup page', () => {
 
-    const browser = new Browser();
-
-    before(function() {
-        return browser.visit('/');
+    before(() => {
+        this.server = http.createServer(app).listen(3000);
     });
 
-    describe('submits form', function() {
-        it('should be successful', function() {
-            browser.assert.success();
+    describe('hello world', () => {
+        let browser = new Browser({ site: 'http://localhost:3000'});
+
+        before((done) => {
+            browser.visit('/', done);
+        });
+
+        describe('/にアクセス出来ること', () =>{
+            it('should be successful', () => {
+                browser.assert.success();
+            });
         });
     });
 
+    after( (done) => {
+        this.server.close(done);
+    });
 });
